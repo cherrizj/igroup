f292ff6b549515ca540dc3a29845327f8ec2f922
hinikai <hinikai@gmail.com>
2017-04-21 22:07:47 +0800

    update

--- client/wechat/pages/index/index.js
+++ client/wechat/pages/index/index.js
@@ -6,54 +6,24 @@ Page({
     motto: 'Wake Up Neo!',
     userInfo: {}
   },
-  //事件处理函数
-  bindViewTap: function() {
+  goToLog: function() {
     wx.navigateTo({
       url: '../logs/logs'
+    })
+  },
+  goToEditor: function() {
+    wx.navigateTo({
+      url: '../editor/editor'
+    })
+  },
+  goToTest: function() {
+    wx.navigateTo({
+      url: '../test/test'
     })
   },
   goToTextarea: function() {
     wx.navigateTo({
       url: '../textarea/textarea'
-    })
-  },
-  scan:function(cb){
-    wx.scanCode({
-      success: (res) => {
-        console.log(res)
-      }
-    })
-  },
-  changeName: function(e) {
-    this.setData({
-      motto: 'Hello iGroup'
-    })
-  },
-
-  getLocation: function(e) {
-    wx.getLocation({
-      type: 'gcj02', //返回可以用于wx.openLocation的经纬度
-      success: function(res) {
-        var latitude = res.latitude
-        var longitude = res.longitude
-        wx.openLocation({
-          latitude: latitude,
-          longitude: longitude,
-          scale: 28
-        })
-      }
-    })
-  },
-
-  onLoad: function () {
-    console.log('onLoad')
-    var that = this
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
-      //更新数据
-      that.setData({
-        userInfo:userInfo
-      })
     })
   }
 })
